5fc67a2fe0c8a785a21cedf5bffc039f9123efc0
Aditya Mishra <aditya.mishra@dynamix.co.in>
2018-08-31 03:03:13 +0530

    [Edit] Minor Mods

--- src/Store/actions/actionTypes.js
+++ src/Store/actions/actionTypes.js
@@ -1,26 +1,26 @@
 //from Auth.actions
-export const AUTH_ERROR = 'ERROR';
-export const AUTH_LOGIN = 'LOGIN';
-export const AUTH_LOGOUT = 'LOGOUT';
-export const AUTH_FETCH_USER = 'FETCH_USER';
+export const AUTH_ERROR = "ERROR";
+export const AUTH_LOGIN = "LOGIN";
+export const AUTH_LOGOUT = "LOGOUT";
+export const AUTH_FETCH_USER = "FETCH_USER";
 
-export const AUTH_FAIL = 'AUTH_FAIL';
-export const AUTH_START = 'AUTH_START';
-export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_FAIL = "AUTH_FAIL";
+export const AUTH_START = "AUTH_START";
+export const AUTH_SUCCESS = "AUTH_SUCCESS";
 
 //from profile actions
-export const PROF_SET_USERDATA = 'SET_USERDATA';
-export const PROF_FETCH_USERDATA = 'FETCH_USERDATA';
-export const PROF_UPDATE_PROGRESS = 'UPDATE_PROGRESS';
-export const DASH_SET_USERDATA = 'SET_USERDATA';
-export const DASH_FETCH_USERDATA = 'FETCH_USERDATA';
-export const DASH_UPDATE_PROGRESS = 'UPDATE_PROGRESS';
-export const DASH_FETCH_DAILY_COUNT = 'FETCH_DAILY_COUNT';
+export const PROF_SET_USERDATA = "SET_USERDATA";
+export const PROF_FETCH_USERDATA = "FETCH_USERDATA";
+export const PROF_UPDATE_PROGRESS = "UPDATE_PROGRESS";
+export const DASH_SET_USERDATA = "SET_USERDATA";
+export const DASH_FETCH_USERDATA = "FETCH_USERDATA";
+export const DASH_UPDATE_PROGRESS = "UPDATE_PROGRESS";
+export const DASH_FETCH_DAILY_COUNT = "FETCH_DAILY_COUNT";
 
 //from article actions
-export const CONT_READ_ARTICLES = 'READ_ARTICLES';
-export const CONT_SET_FAV = 'SET_FAV';
-export const CONT_SET_FILTER = 'SET_FILTER';
-export const CONT_FETCH_ARTICLES = 'FETCH_ARTICLES';
-export const CONT_COMMIT_ARTICLES = 'COMMIT_ARTICLES';
-export const CONT_READ_COUNT = 'READ_COUNT';
+export const CONT_READ_ARTICLES = "READ_ARTICLES";
+export const CONT_SET_FAV = "SET_FAV";
+export const CONT_SET_FILTER = "SET_FILTER";
+export const CONT_FETCH_ARTICLES = "FETCH_ARTICLES";
+export const CONT_COMMIT_ARTICLES = "COMMIT_ARTICLES";
+export const CONT_READ_COUNT = "READ_COUNT";
